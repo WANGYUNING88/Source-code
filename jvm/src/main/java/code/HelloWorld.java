cb16d75771f9ff1b40ce68e6a05bb3bd078d729b
wangyuning88 <19970305.wang>
2020-03-20 12:15:00 +0800

    JVM

---
+++ jvm/src/main/java/code/HelloWorld.java
@@ -0,0 +1,30 @@
+package code;
+
+public class HelloWorld {
+    //定义类属性：常量、变量、成员属性
+    private Object object = new Object();
+    private static int i = 0;
+    private static String s = "hello world";
+
+    /**
+     * 程序入口
+     * @param args
+     */
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        int result = helloWorld.add();
+        System.out.println(result);
+    }
+
+    /**
+     * 定义一个方法 局部变量
+     *
+     * @return
+     */
+    private int add() {
+        int a = 1;
+        int b = 2;
+        int c = (a + b) * 100;
+        return c;
+    }
+}
